6055449ce5e4dba874e22299ff2da4ca26c20ea0
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-05-28 10:52:59 -0300

    feat(Theme): cria e implementa ThemeProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
+import { ProvedorTema } from "./componentes/themeProvider";
 import { Card } from "./componentes/card";
 import Estilos from "./componentes/estilosGlobais/estilos";
 
 function App() {
   return (
-    <>
+    <ProvedorTema>
       <Estilos />
       <Card>
         <h1>freelando</h1>
       </Card>
-    </>
+    </ProvedorTema>
   );
 }
 
--- src/componentes/card/index.jsx
+++ src/componentes/card/index.jsx
@@ -1,8 +1,11 @@
 import styled from "@emotion/styled";
 
 const DivEstilizada = styled.div`
-  /* background: ;
-    border: 1px solid; */
+  background: ${(props) => props.theme.cores.secundarias.a};
+  border: 1px solid;
+  border-color: ${(props) => props.theme.cores.primarias.a};
+  border-radius: ${(props) => props.theme.espacamentos.s};
+  padding: ${(props) => props.theme.espacamentos.l};
 `;
 
 export const Card = ({ children }) => {
