ba144f18f56ebcd75d18d3e410e4fc983dfb2fd4
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-02 09:02:08 -0300

    feat(form): implementa formulário baseando no react grid system

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Card } from "./componentes/card";
 import Estilos from "./componentes/estilosGlobais/estilos";
 import { Tipografia } from "./componentes/tipografia";
 import { CampoTexto } from "./componentes/campoTexto";
+import { Botao } from "./componentes/button";
 import { Col, Container, Row } from "react-grid-system";
 
 function App() {
@@ -22,7 +23,40 @@ function App() {
                 sobre suas demandas e sobre sua forma de trabalho.{" "}
               </Tipografia>
               <form>
-                <CampoTexto titulo="Nome Completo"></CampoTexto>
+                <Row>
+                  <Col>
+                    <CampoTexto titulo="Nome Completo"></CampoTexto>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={4} md={4} sm={4}>
+                    <CampoTexto titulo="Estado"></CampoTexto>
+                  </Col>
+                  <Col lg={8} md={8} sm={8}>
+                    <CampoTexto titulo="Cidade"></CampoTexto>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <CampoTexto titulo="E-mail"></CampoTexto>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={6} md={6} sm={6}>
+                    <CampoTexto titulo="Senha"></CampoTexto>
+                  </Col>
+                  <Col lg={6} md={6} sm={6}>
+                    <CampoTexto titulo="Repita a Senha"></CampoTexto>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={6} md={6} sm={6} style={{ textAlign: "left" }}>
+                    <Botao variante="secundaria">Anterior</Botao>
+                  </Col>
+                  <Col lg={6} md={6} sm={6} style={{ textAlign: "right" }}>
+                    <Botao>Próxima</Botao>
+                  </Col>
+                </Row>
               </form>
             </Card>
           </Col>
