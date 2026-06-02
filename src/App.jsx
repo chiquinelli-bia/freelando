192c43aabea5d530218b4cb1d87e391073f216e7
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-02 08:46:00 -0300

    feat(dependecies): instala biblioteca react grid system e implementa

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,31 @@ import { Card } from "./componentes/card";
 import Estilos from "./componentes/estilosGlobais/estilos";
 import { Tipografia } from "./componentes/tipografia";
 import { CampoTexto } from "./componentes/campoTexto";
+import { Col, Container, Row } from "react-grid-system";
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
-      <Card>
-        <Tipografia variante="h1" componente="h1">
-          Crie seu Cadastro.
-        </Tipografia>
-        <Tipografia variante="body" componente="body">
-          Crie seu perfil gratuitamente para começar a trabalhar com os melhores
-          freelancers. Em seguida, você poderá dar mais detalhes sobre suas
-          demandas e sobre sua forma de trabalho.{" "}
-        </Tipografia>
-        <form>
-          <CampoTexto titulo="Nome Completo"></CampoTexto>
-        </form>
-      </Card>
+      <Container>
+        <Row justify="center">
+          <Col lg={6} md={8} sm={12}>
+            <Card>
+              <Tipografia variante="h1" componente="h1">
+                Crie seu Cadastro.
+              </Tipografia>
+              <Tipografia variante="body" componente="body">
+                Crie seu perfil gratuitamente para começar a trabalhar com os
+                melhores freelancers. Em seguida, você poderá dar mais detalhes
+                sobre suas demandas e sobre sua forma de trabalho.{" "}
+              </Tipografia>
+              <form>
+                <CampoTexto titulo="Nome Completo"></CampoTexto>
+              </form>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </ProvedorTema>
   );
 }
