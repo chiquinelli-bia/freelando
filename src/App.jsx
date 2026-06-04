a34caeb7f7b7d937c678f945e31c51b429eda468
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-04 18:21:11 -0300

    feat(app): importa componente listaSuspensa e cria array de estados

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,44 @@ import { Botao } from "./componentes/button";
 import { Col, Container, Row } from "react-grid-system";
 import Footer from "./componentes/rodape";
 import Header from "./componentes/header";
+import { ListaSuspensa } from "./componentes/listaSuspensa";
+
+const estadosBrasileiros = [
+  { text: "Acre", value: "AC" },
+  { text: "Alagoas", value: "AL" },
+  { text: "Amapá", value: "AP" },
+  { text: "Amazonas", value: "AM" },
+  { text: "Bahia", value: "BA" },
+  { text: "Ceará", value: "CE" },
+  { text: "Distrito Federal", value: "DF" },
+  { text: "Espírito Santo", value: "ES" },
+  { text: "Goiás", value: "GO" },
+  { text: "Maranhão", value: "MA" },
+  { text: "Mato Grosso", value: "MT" },
+  { text: "Mato Grosso do Sul", value: "MS" },
+  { text: "Minas Gerais", value: "MG" },
+  { text: "Pará", value: "PA" },
+  { text: "Paraíba", value: "PB" },
+  { text: "Paraná", value: "PR" },
+  { text: "Pernambuco", value: "PE" },
+  { text: "Piauí", value: "PI" },
+  { text: "Rio de Janeiro", value: "RJ" },
+  { text: "Rio Grande do Norte", value: "RN" },
+  { text: "Rio Grande do Sul", value: "RS" },
+  { text: "Rondônia", value: "RO" },
+  { text: "Roraima", value: "RR" },
+  { text: "Santa Catarina", value: "SC" },
+  { text: "São Paulo", value: "SP" },
+  { text: "Sergipe", value: "SE" },
+  { text: "Tocantins", value: "TO" },
+];
 
 function App() {
   return (
     <ProvedorTema>
       <Estilos />
       <Header />
-      <Container style={{ margin: "80px auto" }}>
+      <Container style={{ margin: "80px 0", maxWidth: "auto" }}>
         <Row justify="center">
           <Col lg={6} md={8} sm={12}>
             <Card>
@@ -33,7 +64,10 @@ function App() {
                 </Row>
                 <Row>
                   <Col lg={4} md={4} sm={4}>
-                    <CampoTexto titulo="Estado"></CampoTexto>
+                    <ListaSuspensa
+                      titulo="Estado"
+                      opcoes={estadosBrasileiros}
+                    />
                   </Col>
                   <Col lg={8} md={8} sm={8}>
                     <CampoTexto titulo="Cidade"></CampoTexto>
