811433434b302dd7e8bef84648402dab133eae4b
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-08 10:47:56 -0300

    feat(router): implementa rota para a página cadastro

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,9 +1,41 @@
 import { createBrowserRouter } from "react-router-dom";
-import PaginaInicial from "../paginas/pngInicial";
+import { LayoutBase } from "../paginas/layoutBase";
+import { LayoutBaseCadastro } from "../paginas/cadastro/layoutBase";
+import { SelecaoCliente } from "../paginas/cadastro/selecaoCliente";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <PaginaInicial />,
+    element: <LayoutBase />,
+    children: [
+      {
+        path: "cadastro",
+        element: <LayoutBaseCadastro />,
+        children: [
+          {
+            path: "",
+            element: <SelecaoCliente />,
+            children: [
+              {
+                path: "",
+                element: <SelecaoCliente />,
+              },
+              {
+                path: "cliente",
+                element: <h1>Interesesses</h1>,
+              },
+              {
+                path: "dados-pessoais",
+                element: <h1>Dados pessoais</h1>,
+              },
+              {
+                path: "concluido",
+                element: <h1>Concluído</h1>,
+              },
+            ],
+          },
+        ],
+      },
+    ],
   },
 ]);
