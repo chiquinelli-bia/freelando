3dcf95771e34ed238bff34969e9970f1779a51b0
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-14 12:53:15 -0300

    feat(layoutBase): importa provider

--- src/paginas/cadastro/layoutBase.jsx
+++ src/paginas/cadastro/layoutBase.jsx
@@ -1,23 +1,26 @@
 import { Col, Row } from "react-grid-system";
 import { Outlet } from "react-router-dom";
 import { Card } from "../../componentes/card";
+import { CadastroUsuarioProvider } from "../../contexto/cadastroUsuario";
 
 export const LayoutBaseCadastro = () => {
   return (
-    <Row justify="center" style={{ marginRight: "0" }}>
-      <Col
-        xxx={6}
-        xxl={6}
-        xl={6}
-        lg={6}
-        md={8}
-        sm={12}
-        style={{ margin: "80px 0" }}
-      >
-        <Card>
-          <Outlet />
-        </Card>
-      </Col>
-    </Row>
+    <CadastroUsuarioProvider>
+      <Row justify="center" style={{ marginRight: "0" }}>
+        <Col
+          xxx={6}
+          xxl={6}
+          xl={6}
+          lg={6}
+          md={8}
+          sm={12}
+          style={{ margin: "80px 0" }}
+        >
+          <Card>
+            <Outlet />
+          </Card>
+        </Col>
+      </Row>
+    </CadastroUsuarioProvider>
   );
 };
