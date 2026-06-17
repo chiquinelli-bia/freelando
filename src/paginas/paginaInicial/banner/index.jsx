313bea252e9b0b2c6cf8db3a2db6df0e70ae20ea
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-17 10:15:06 -0300

    feat(Banner): cria componente Banner

---
+++ src/paginas/paginaInicial/banner/index.jsx
@@ -0,0 +1,36 @@
+import { Col, Container, Row } from "react-grid-system";
+import { Tipografia } from "../../../componentes/tipografia";
+import { Botao } from "../../../componentes/button";
+import imgBanner from "./banner.png";
+import styled from "@emotion/styled";
+
+const ImgEstilizado = styled.img`
+  max-width: 100%;
+`;
+const DivEstilizada = styled.div`
+  padding: ${(props) => props.theme.espacamentos.l};
+  background: ${(props) => props.theme.cores.neutras.c};
+  color: ${(props) => props.theme.cores.primarias.b};
+  margin: 0;
+`;
+
+export const Banner = () => {
+  return (
+    <DivEstilizada>
+      <Container>
+        <Row>
+          <Col md={5} sm={12} style={{ textAlign: "left" }}>
+            <Tipografia variante="h1" componente="h1">
+              Uma ponte entre os freelas mais talentosos e os projetos mais
+              interessantes!
+            </Tipografia>
+            <Botao variante="primaria">Quero me cadastrar!</Botao>
+          </Col>
+          <Col md={7} sm={12}>
+            <ImgEstilizado src={imgBanner} alt="" />
+          </Col>
+        </Row>
+      </Container>
+    </DivEstilizada>
+  );
+};
