9ed3d8546f2869932613b3054d69e4261226fd25
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-17 10:07:15 -0300

    refactor(Card): refatora componente Card para futuras implementações

--- src/componentes/card/index.jsx
+++ src/componentes/card/index.jsx
@@ -1,14 +1,28 @@
 import styled from "@emotion/styled";
 
 const DivEstilizada = styled.div`
-  background: ${(props) => props.theme.cores.secundarias.a};
-  border: 1px solid;
-  border-color: ${(props) => props.theme.cores.primarias.a};
+  border: ${(props) => (props.$borda ? "1px solid" : "none")};
   border-radius: ${(props) => props.theme.espacamentos.s};
   padding: ${(props) => props.theme.espacamentos.l};
   text-align: center;
+  background: ${(props) =>
+    props.variante === "primaria"
+      ? props.theme.cores.secundarias.a
+      : props.theme.cores.secundarias.c};
+  border-color: ${(props) =>
+    props.variante === "primaria"
+      ? props.theme.cores.primarias.a
+      : props.theme.cores.primarias.c};
 `;
 
-export const Card = ({ children }) => {
-  return <DivEstilizada>{children}</DivEstilizada>;
+export const Card = ({
+  children,
+  comBorda = "true",
+  variante = "primaria",
+}) => {
+  return (
+    <DivEstilizada $borda={comBorda} variante={variante}>
+      {children}
+    </DivEstilizada>
+  );
 };
