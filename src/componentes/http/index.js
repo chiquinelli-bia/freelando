e6eea0ebfe4262d518a13f472bd56f12ed606e2e
Bianca Chiquinelli <bchiquinelli24@gmail.com>
2026-06-26 09:20:25 -0300

    feat(http): adiciona interceptador a requisiçoes

--- src/componentes/http/index.js
+++ src/componentes/http/index.js
@@ -1,6 +1,21 @@
 import axios from "axios";
+import armazenadorToken from "../../utils/armazenadorToken";
 
 const http = axios.create({
   baseURL: "http://localhost:8080/",
 });
+
+http.interceptors.request.use(
+  function (config) {
+    const token = armazenadorToken.accessToken;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+      console.log("Token foi adicionado ao cabeçalho");
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
 export default http;
